fd4fcbd938dd10a0c4b3f569083202786759171d
Dustin <dustingreelis@gmail.com>
2022-10-10 20:31:05 -0500

    uploading files

--- client.py
+++ client.py
@@ -14,9 +14,7 @@ blue = (0, 0, 128)
 pygame.font.init()
 
 
-
-
-def redrawWindow(win,player):
+def redrawWindow(win, player):
     win.fill(white)
     font = pygame.font.Font('freesansbold.ttf', 15)
     text = font.render(str(player), True, green, blue)
@@ -30,43 +28,57 @@ def main():
     n = Network()
     p = n.getP()
     clock = pygame.time.Clock()
+    drafted_cards = []
 
     while run:
         clock.tick(30)
-
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
                 pygame.quit()
 
-
-
             if event.type == pygame.KEYDOWN:
+                # set that card number to 0
                 if event.key == pygame.K_KP_0:
-                    p[1][0] = 0
-                    response = [0]
+                    print(p)
+                    print(p[3][0])
+                    drafted_cards.append(p[3][0])
+                    p[3][0] = 0
                 if event.key == pygame.K_KP_1:
-                    p[1][1] = 0
-                    response = [1]
+                    drafted_cards.append(p[3][1])
+                    p[3][1] = 0
                 if event.key == pygame.K_KP_2:
-                    p[1][2] = 0
+                    drafted_cards.append(p[3][2])
+                    p[3][2] = 0
                 if event.key == pygame.K_KP_3:
-                    p[1][3] = 0
+                    drafted_cards.append(p[3][3])
+                    p[3][3] = 0
                 if event.key == pygame.K_KP_4:
-                    p[1][4] = 0
+                    drafted_cards.append(p[3][4])
+                    p[3][4] = 0
                 if event.key == pygame.K_KP_5:
-                    p[1][5] = 0
+                    drafted_cards.append(p[3][5])
+                    p[3][5] = 0
                 if event.key == pygame.K_KP_6:
-                    p[1][6] = 0
+                    drafted_cards.append(p[3][6])
+                    p[3][6] = 0
                 if event.key == pygame.K_KP_7:
-                    p[1][7] = 0
+                    drafted_cards.append(p[3][7])
+                    p[3][7] = 0
                 if event.key == pygame.K_KP_8:
-                    p[1][8] = 0
+                    drafted_cards.append(p[3][8])
+                    p[3][8] = 0
                 if event.key == pygame.K_KP_9:
-                    p[1][9] = 0
+                    drafted_cards.append(p[3][9])
+                    p[3][9] = 0
+                # played = 1
+                p[2] = 1
+                print(drafted_cards)
+                print(p)
                 n.send(p)
-
         redrawWindow(win, p)
+
+
 if __name__ == '__main__':
     main()
--- server.py
+++ server.py
@@ -2,13 +2,16 @@ import socket
 from _thread import *
 import pickle
 import random
+import numpy as np
+
 
 server = "192.168.1.11"
 port = 5555
 
-expected_players = 0
-# cube_length = expected_players*15*3
-cube_length = 135
+human_players = 2
+AI_players = 0
+total_players = human_players + AI_players
+cube_length = (total_players)*15*3
 cube = list(range(cube_length))
 random.shuffle(cube)
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -23,45 +26,58 @@ s.listen(2)
 print("Waiting for a connection, Server Started")
 
 
-def threaded_client(conn, player, exp_players, main_loop, cube1):
+def trade_packs(player_num, recd_pack_p_num, recd_pack):
+    pass
+
+
+def threaded_client(conn, player, hum_players, tot_players, main_loop, cube1):
     cube_new = [cube1[i:i + 15] for i in range(0, len(cube1), 15)]
-    pack = [player, cube_new[player]]
+    played = 0
+    pass_to = player
+    allWent = np.zeros((tot_players,), dtype=int)
+    pack = [player, pass_to, played, cube_new[player]]
     msg = pickle.dumps(pack)
     conn.send(msg)
 
 
     reply = ""
-    if player <= exp_players:
-        main_loop = True
-        while main_loop:
-            try:
+    # if player == hum_players:
+    main_loop = True
+    while main_loop:
+        try:
+            # whenever a pack is sent by a client
+            data = pickle.loads(conn.recv(2048))
+            # change that player's value to a 1 (to indicate they went)
+            allWent[data[0]] = 1
+            # when all players have gone...
+            if allWent == np.ones(tot_players):
+                # start "trade_packs"
+                trade_packs(player, data[0], data[1])
+                # then set all the Went values back to 0
+                allWent = np.zeros(tot_players)
 
-                data = pickle.loads(conn.recv(2048))
 
-                if not data:
-                    print("Disconnected")
-                    break
-                else:
-                    # if player == 1:
-                        # reply = players[0]
-                    # else:
-                        # reply = players[1]
-
-                    print("Received: ", data)
-                    print("Sending : ", reply)
-
-                conn.sendall(pickle.dumps(reply))
-            except:
+            if not data:
+                print("Disconnected")
                 break
+            else:
 
-        print("Lost connection")
-        conn.close()
+               print("Received: ", data)
+               print("Sending : ", reply)
+
+            conn.sendall(pickle.dumps(reply))
+        except:
+            break
+
+    print("Lost connection")
+    conn.close()
 
 
-currentPlayer = 0
+
+currentPlayer = 1
 while True:
     conn, addr = s.accept()
     print("Connected to:", addr)
 
-    start_new_thread(threaded_client, (conn, currentPlayer, expected_players, loop, cube))
+    start_new_thread(threaded_client, (conn, currentPlayer, human_players, total_players, loop, cube))
     currentPlayer += 1
